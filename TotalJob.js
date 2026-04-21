1834aa1a88df8ea15920dde4ac38fae2e8d1e241
hasanimam98 <hasanimam.ict@gmail.com>
2026-04-20 20:20:58 -0400

    modified

--- TotalJob.js
+++ TotalJob.js
@@ -1,6 +1,3 @@
-
-// let cards = document.querySelectorAll(".card-body");
-// let total = cards.length;
 function updateCount(){
   const total = document.querySelectorAll(".card-body").length;
 
@@ -11,69 +8,7 @@ document.querySelectorAll('.card-body').forEach((card, index) => {
   card.dataset.id = index;
 });
 
-// Total-count
-// document.getElementById("total-card").innerText = total;
-// document.getElementById("total-card2").innerText = total;
 
-
-
-// // INTERVIEW buttons
-// const interviewButtons = document.querySelectorAll('.Interview');
-// interviewButtons.forEach(function(btn){
-//   btn.addEventListener('click', function(){
-
-
-
-//     const appliedBtn = btn.closest('.card-body').querySelector('.Not-Applied');
-//     appliedBtn.innerText = "APPLIED";
-//     appliedBtn.classList.remove("btn-active");
-//     appliedBtn.classList.add("btn-success");
-  
-// const Tab2 = document.querySelector('.Applied2');
-// const card = btn.closest('.card-body');
-//     const cardClone = card.cloneNode(true);
-
-//     Tab2.appendChild(cardClone);
-//     // Tab2.appendChild(card);
-   
-//   });
-  
-// });
-
-// // REJECTED buttons
-// const rejectedButtons = document.querySelectorAll('.rejected');
-// rejectedButtons.forEach(function(btn){
-//   btn.addEventListener('click', function(){
-//     const appliedBtn = btn.closest('.card-body').querySelector('.Not-Applied');
-//     appliedBtn.innerText = "REJECTED";
-//     appliedBtn.classList.remove("btn-secondary");
-//     appliedBtn.classList.add("btn-error");
-
-// //     const cardRej = btn.closest('.card-body');
-// // const Tab3 = document.querySelector('.Rejected2');
-// // Tab3.appendChild(cardRej);
-// const Tab3 = document.querySelector('.Rejected2');
-// const card2 = btn.closest('.card-body');
-//     const cardClone2 = card2.cloneNode(true);
-
-//     Tab3.appendChild(cardClone2);
-//     // Tab3.appendChild(card);
-    
-//   });
-// });
-
-
-// // DELETE-BUTTON
-
-// // const deleteButtons = document.querySelectorAll('.dlt');
-
-// // deleteButtons.forEach(function(btn){
-// // btn.addEventListener('click', function(){
-// //   const card = btn.closest('.card-body');
-// // card.remove(); 
-
-// // });
-// // });
 
 document.addEventListener('click', function(e){
 
@@ -90,7 +25,7 @@ document.addEventListener('click', function(e){
     const Tab2 = document.querySelector('.Applied2');
     const cardClone = card.cloneNode(true);
 
-    //id copy
+   
     cardClone.dataset.id = card.dataset.id;
 
     Tab2.appendChild(cardClone);
@@ -109,7 +44,6 @@ document.addEventListener('click', function(e){
     const Tab3 = document.querySelector('.Rejected2');
     const cardClone2 = card.cloneNode(true);
 
-    // 
     cardClone2.dataset.id = card.dataset.id;
 
     Tab3.appendChild(cardClone2);
