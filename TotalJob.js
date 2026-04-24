ac17bf07f37259a9952f5784ede0bb02f4588bd5
hasanimam98 <hasanimam.ict@gmail.com>
2026-04-23 20:52:29 -0400

    modified

--- TotalJob.js
+++ TotalJob.js
@@ -1,12 +1,12 @@
+
 function updateCount(){
-  const total = document.querySelectorAll(".card-body").length;
+  const total = document.querySelectorAll("#mainJobs .card-body").length;
 
   document.getElementById("total-card").innerText = total;
   document.getElementById("total-card2").innerText = total;
 }
-document.querySelectorAll('.card-body').forEach((card, index) => {
-  card.dataset.id = index;
-});
+
+
 
 
 
@@ -25,11 +25,14 @@ document.addEventListener('click', function(e){
     const Tab2 = document.querySelector('.Applied2');
     const cardClone = card.cloneNode(true);
 
-   
-    cardClone.dataset.id = card.dataset.id;
+   cardClone.dataset.id = card.dataset.id;
+    
 
     Tab2.appendChild(cardClone);
+   
+    
   }
+  
 
   // REJECTED
   if(e.target.closest('.rejected')){
@@ -47,18 +50,29 @@ document.addEventListener('click', function(e){
     cardClone2.dataset.id = card.dataset.id;
 
     Tab3.appendChild(cardClone2);
+    // Tab3.appendChild(card);
+    
+    
   }
 
   // DELETE
-  if(e.target.closest('.dlt')){
-    const card = e.target.closest('.card-body');
-    const id = card.dataset.id;
+if(e.target.closest('.dlt')){
+  const card = e.target.closest('.card-body');
+ const id = card.dataset.id;
 
-    document.querySelectorAll(`[data-id="${id}"]`).forEach(el => {
-      el.remove();
-    });
+   document.querySelectorAll(`[data-id="${id}"]`).forEach(el => {
+    el.remove();
+    
+  });
+  
     updateCount();
-  }
+    
+ }
+
+
   
 
-});
+ });
+ updateCount();
+
+
